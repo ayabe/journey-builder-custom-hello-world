20cbeeb4aa172469a36806255e8b54c445a7221b
ayabe <kayabe@salesforce.com>
2016-07-14 15:52:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ var app = express();
 // Register configs for the environments where the app functions
 // , these can be stored in a separate file using a module like config
 var APIKeys = {
-    appId           : '__insert_your_app_id__',
-    clientId        : '__insert_your_app_client_id__',
-    clientSecret    : '__insert_your_app_client_secret__',
-    appSignature    : '__insert_your_app_signature__',
+    appId           : '1742a858-ed67-46a6-9559-b678c0bbfe5f',
+    clientId        : 'l94e03x0ayp8eydvjydaunhe',
+    clientSecret    : 'Szkcuuzr0CSDBD9XlATK6eDL',
+    appSignature    : 'uozcimdsv5oobjtrhufsmphchxy3cznkxqrhxrfu5p0pal211gddkidk5hvwqc0xm1w1vybz4d3lb3qnh3h1ybeiosvge2il2mxwjsa0304mnnhp0d0z4hijad5plqv5fdzi4a1lxxvcduq0ov5a5vnh01izm0qksrxxiriqa2tpguox4sdo2wnmcx4nbhlxrsj2rhds2e21sg2altcngtidyk4tykczsrawe5o1ummariavmnwrnuski1qcd2q',
     authUrl         : 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
 };
 
